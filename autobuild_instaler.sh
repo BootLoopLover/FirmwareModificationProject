1dca7a453f52326a19704d6fb39b3261cde85072
BootLoopLover <zuhdimansor88@gmail.com>
2025-06-19 22:46:53 +0800

    Update autobuild_instaler.sh

--- autobuild_instaler.sh
+++ autobuild_instaler.sh
@@ -39,7 +39,7 @@ show_banner() {
   / _ \_______    (_)__ ____/ /_       
  / ___/ __/ _ \  / / -_) __/ __/  _ _ _ 
 /_/  /_/  \___/_/ /\__/\__/\__/  (_|_|_)
-             |___/ 
+             |___/ © Project by Pakalolo
 EOF
     echo -e "${NC}"
     for i in $(seq 1 60); do echo -ne "${BLUE}-${NC}"; sleep 0.005; done
